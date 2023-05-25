8b29249dd18ea444f76e65f3bd0ca7e8a5add939
Parker Parham <parkerparham@gmail.com>
2023-05-25 11:48:17 -0400

    Adds page view tracking session and routes to get articles and articles by id

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 from models import db, Article, User
 
@@ -10,6 +11,7 @@ app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+CORS(app)
 
 migrate = Migrate(app, db)
 
@@ -22,13 +24,20 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = [article.to_dict() for article in Article.query.all()]
+    return make_response(jsonify(articles), 200)
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1
+    if session['page_views'] <= 3:
+        print(session['page_views'])
+        article = Article.query.filter_by(id=id).first().to_dict()
+        return make_response(jsonify(article), 200)
 
-    pass
+    else:
+        return make_response({'message': "Maximum pageview limit reached"}, 401)
 
 if __name__ == '__main__':
     app.run(port=5555)
